0e9a42df199ff9950cc4b1f512d772a92c4714b4
Yoshio Kimura <ykimura@studio-parsec.co.jp>
2025-02-04 20:54:07 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-indiclient",
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v18)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-indiclient",
+            targets: ["swift-indiclient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "swift-indiclient",
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "DequeModule", package: "swift-collections")
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
