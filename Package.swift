8f5829ef523e39ec80d27256b7adfe31157fae1c
Yoshio Kimura <ykimura@studio-parsec.co.jp>
2025-02-13 09:27:17 +0900

    Target name collected

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-indiclient",
+            name: "INDIClient",
             dependencies: [
                 .product(name: "Collections", package: "swift-collections"),
                 .product(name: "DequeModule", package: "swift-collections")
